eedc0d5b2cdab5ce6c564ab01d09215dfc145382
Simon O'Rorke <simon.ororke@outlook.com>
2025-10-24 09:49:11 +1300

    spin-sleep with comments on other options

--- Rust/millisecond_ticker/src/ticker.rs
+++ Rust/millisecond_ticker/src/ticker.rs
@@ -1,11 +1,24 @@
-﻿// use howlong::{Clock, SteadyClock};
-// use spinwait::SpinWait;
-use std::sync::Arc;
+﻿use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
 
 /// A steady ticker that asynchronously calls a callback on ticking
 /// and can be started and stopped.
+/// Timer: spin_sleep::sleep
+///     Benchmark test with 1-ms interval specified:
+///         Average actual interval: 1.00196 ms
+///         CPU usage: 2-3%
+/// Other steady timers tested and commented out:
+/// SteadyClock with SpinWait
+///     Benchmark test with 1-ms interval specified:
+///         Average actual interval: 1.00175 ms
+///         CPU usage: 4-9%
+/// std::thread::sleep
+///     Benchmark test with 1-ms interval specified:
+///         Average actual interval: 1.51934 ms
+///         CPU usage: 0-3%
+/// Benchmark tests were run on a Windows PC with
+/// an AMD Ryzen 9 5900X 12-Core processor.
 pub struct Ticker {
     interval: Duration,
     running: Arc<AtomicBool>,
@@ -19,7 +32,7 @@ impl Ticker {
         }
     }
 
-    /// Starts the ticker with a callback
+    /// Starts the ticker with a callback.
     pub fn start<F>(&mut self, callback: F)
     where
         F: Fn() + Send + Clone + 'static,
@@ -36,8 +49,8 @@ impl Ticker {
         // in advance.
         rayon::spawn(move || {
             while running.load(Ordering::SeqCst) {
-                std::thread::sleep(interval);
-                // spin_sleep::sleep(interval);
+                // std::thread::sleep(interval);
+                spin_sleep::sleep(interval);
                 // let interval_start = SteadyClock::now();
                 // spinner.spin_until(|| (SteadyClock::now() - interval_start) >= interval);
                 let callback_clone = callback.clone();
@@ -46,7 +59,7 @@ impl Ticker {
         });
     }
 
-    /// Stops the ticker
+    /// Stops the ticker.
     pub fn stop(&self) {
         self.running.store(false, Ordering::SeqCst);
     }
