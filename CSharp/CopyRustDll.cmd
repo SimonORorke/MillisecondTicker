27d0e88cddc6a9776d269101bf0668ef04d1bcac
Simon O'Rorke <simon.ororke@outlook.com>
2025-10-25 08:30:21 +1300

    CopyRustDll.cmd

---
+++ CSharp/CopyRustDll.cmd
@@ -0,0 +1,5 @@
+copy ..\Rust\millisecond_ticker\target\release\millisecond_ticker.dll Example\bin\Debug\net9.0 /y
+copy ..\Rust\millisecond_ticker\target\release\millisecond_ticker.dll Example\bin\Release\net9.0 /y
+copy ..\Rust\millisecond_ticker\target\release\millisecond_ticker.dll Tests\bin\x64\Debug\net9.0 /y
+copy ..\Rust\millisecond_ticker\target\release\millisecond_ticker.dll Tests\bin\x64\Release\net9.0 /y
+copy ..\Rust\millisecond_ticker\target\release\millisecond_ticker.dll RustLibrary /y
